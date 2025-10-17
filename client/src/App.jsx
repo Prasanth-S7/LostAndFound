aa3d27d812456ef07f08c8f210d3b6a8f9d71f29
prasanth <prasanthsampath2005@gmail.com>
2025-10-17 19:02:08 +0530

    checking if client ci runs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
 
-const App = () => (
+const App = () => ( //is it working
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <Toaster />
